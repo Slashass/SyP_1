efcc5c166214168b4c565eb148baefd3a1fdda55
Slashass <70883938+Slashass@users.noreply.github.com>
2021-01-26 14:42:47 +0200

    upe

---
+++ src/Controller/UpesController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class UpesController extends AbstractController
+{
+    public function bebras(string $pasveikinimas, int $kiekis): Response
+    {
+
+        $udraUrl = $this->generateUrl('udra');
+        // $order = $r->query->get('order');
+
+        return $this->render('upe/bebras.html.twig', [
+            'say' => $pasveikinimas,
+            'count' => $kiekis,
+            'go' => $udraUrl
+        ]);
+    }
+    public function udra(): Response
+    {
+
+        $bebrasUrl = $this->generateUrl('bebras', ['pasveikinimas' => 'Viso-Gero', 'kiekis' => 100]);
+
+        return $this->render('upe/udra.html.twig', [
+            'go' => $bebrasUrl
+        ]);
+    }
+}
